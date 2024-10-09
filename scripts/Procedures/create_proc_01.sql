a453a4aee2f3985c72900292c4256b32847ed05b
Amy Smith <asmith@liquibase.com>
2024-10-09 10:21:54 -0500

    Removed schema name

--- scripts/Procedures/create_proc_01.sql
+++ scripts/Procedures/create_proc_01.sql
@@ -1,12 +1,12 @@
 --liquibase formatted sql
 --changeset asmith:01 labels:POCEnv
 
-CREATE OR REPLACE PROCEDURE TEST_SET_01.test_proc_01()
+CREATE OR REPLACE PROCEDURE test_proc_01()
 BEGIN
   DECLARE id STRING;
   SET id = GENERATE_UUID();
-  INSERT INTO TEST_SET_01.table_01 (id,name,location)
+  INSERT INTO table_01 (id,name,location)
     VALUES(id, 'Amy','Test');
   SELECT FORMAT("Created row %s", id);
 END
---rollback DROP PROCEDURE TEST_SET_01.test_proc_01;
+--rollback DROP PROCEDURE test_proc_01;
--- scripts/Views/table_01_vw.sql
+++ scripts/Views/table_01_vw.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 --changeset asmith:01 labels:POCEnv
 
-CREATE OR REPLACE VIEW TEST_SET_01.table_01_vw(id, name) AS (
+CREATE OR REPLACE VIEW table_01_vw(id, name) AS (
   SELECT
     id, name
   FROM
-    TEST_SET_01.table_01
+    table_01
   ORDER BY
     id, name DESC
 );
 
---rollback DROP VIEW TEST_SET_01.table_01_vw
+--rollback DROP VIEW table_01_vw
