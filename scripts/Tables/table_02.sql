88b6e935be8fcb5a05ceb64ee6a6dffb5b11f09c
Amy Smith <asmith@liquibase.com>
2024-10-10 16:55:26 -0500

    Fixed tables

--- scripts/Tables/table_02.sql
+++ scripts/Tables/table_02.sql
@@ -1,16 +1,9 @@
 --liquibase formatted sql
 --changeset amy_smith:01 labels:POCEnv
 
-create table table_01
-(
-id string(32),
-name  STRING(40),
-location STRING(10),
-GRANT_ID STRING(10),
-PARTC_ID STRING(10),
-AWRD_ID STRING(10),
-DW_EFF_DT DATE,
-DW_EXPR_DT DATE
-)
-;
---rollback drop table table_01
+create table dbo.table_02 (
+  id int, 
+  name varchar(50),
+  sku varchar(30) 
+);
+--rollback drop table dbo.table_02
