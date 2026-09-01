ceb0c6562f136e914eaf83e6641a2316ed3f25e4
Amy Smith <asmith@liquibase.com>
2026-09-01 11:17:43 -0500

    Adding changes

--- scripts/Tables/table_15.sql
+++ scripts/Tables/table_15.sql
@@ -14,12 +14,19 @@ create table dbo.table_15 (
 --changeset amy_smith:01-alter
 ALTER TABLE dbo.table_15
 ADD status_01 char(1);
---rollback ALTER TABLE dbo.table_15 drop status_01;
+--rollback ALTER TABLE dbo.table_15 drop column status_01;
 
 --changeset amy_smith:02-alter
 ALTER TABLE dbo.table_15
 ADD status_02 char(1);
+--rollback ALTER TABLE dbo.table_15 drop column status_02;
 
 --changeset amy_smith:03-alter
 ALTER TABLE dbo.table_15
 ADD status_03 char(1);
+--rollback ALTER TABLE dbo.table_15 drop column status_03;
+
+--changeset amy_smith:04-alter labels:table15
+ALTER TABLE dbo.table_15
+ADD status_04 char(1);
+--rollback ALTER TABLE dbo.table_15 drop column status_04;
